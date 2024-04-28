119eec3b5a3148d3c5083202d907c3506ea9e888
0334321-LSE <lucasaverio.esposito@students.uniroma2.eu>
2024-04-28 20:41:59 +0200

    Remove some tests

--- ReferenceMonitor/user/compile.sh
+++
@@ -1,3 +0,0 @@
-gcc -o logfile_threads logfile_threads.c -lpthread -fsanitize=address
-gcc -o baucotest baucotest.c -lpthread -fsanitize=address
-gcc -o listhead_threads listhead_threads.c -lpthread -fsanitize=address
